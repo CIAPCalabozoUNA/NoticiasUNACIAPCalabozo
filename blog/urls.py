6236ea2a2a061c1b7a721ce030c1d431aab2ecb7
Nad GarDez <iranad@hotmail.com>
2024-04-18 17:31:59 -0400

    added some views

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index , name= 'index')
+    path('', views.index , name= 'index'),
+    path('article/<int:article_id>/', views.article , name= 'article'),
+    path('article_list/', views.articleList , name= 'article_list'),
+    path('about/', views.about , name= 'about'),
+    path('help_us/', views.helpUs , name= 'help_us'),
+    path('resource/<int:resourse_id>/', views.resource , name= 'resource'),
+    path('author/<int:author_id>/', views.author , name= 'author'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,45 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 def index(request):
-    return HttpResponse('hello world')
+    template = loader.get_template("pages/index.html")
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def article(request, article_id):
+    template = loader.get_template("pages/article.html")
+    context = {
+        'article_id':article_id
+    }
+    return HttpResponse(template.render(context, request))
+
+def articleList(request):
+    template = loader.get_template("pages/articleList.html")
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def about(request):
+    template = loader.get_template("pages/about.html")
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def helpUs(request):
+    template = loader.get_template("pages/helpUs.html")
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def resource(request, resource_id):
+    template = loader.get_template("pages/resource.html")
+    context = {
+        'resource_id': resource_id
+    }
+    return HttpResponse(template.render(context, request))
+
+def author(request, author_id):
+    template = loader.get_template("pages/author.html")
+    context = {
+        'author_id': author_id
+    }
+    return HttpResponse(template.render(context, request))
